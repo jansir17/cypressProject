b505bcd8e3f8da9951311c57296592f11962c8ab
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-07-19 18:02:18 +0600

    mochawesome-report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,31 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/report/mochawesome-report",
+    charts: true,
+    html: true,
+    json: false,
+    overwrite: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
